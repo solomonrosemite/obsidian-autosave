8bd581dbf2b39c9307d4878594c90ab605a661e3
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2025-05-17 23:10:41 +0200

    feat: add explicit autosave

--- main.ts
+++ main.ts
@@ -1,85 +1,61 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, Notice, MarkdownView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface AutoSaveSettings {
+	debounceTime: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AutoSaveSettings = {
+	debounceTime: 3,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoSavePlugin extends Plugin {
+	settings: AutoSaveSettings;
+	private debounceTimer: NodeJS.Timeout | null = null;
 
 	async onload() {
+		console.log('Loading Auto Save Plugin');
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new AutoSaveSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', () => {
+				if (this.debounceTimer) clearTimeout(this.debounceTimer);
+				this.debounceTimer = null;
+			})
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(
+			this.app.workspace.on('editor-change', () => {
+				this.triggerSaveWithDebounce();
+			})
+		);
 	}
 
 	onunload() {
+		console.log('Unloading Auto Save Plugin');
+		if (this.debounceTimer) {
+			clearTimeout(this.debounceTimer);
+			this.debounceTimer = null;
+		}
+	}
 
+	triggerSaveWithDebounce() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (this.debounceTimer) {
+			clearTimeout(this.debounceTimer);
+		}
+
+		this.debounceTimer = setTimeout(async () => {
+			const newActiveFile = this.app.workspace.getActiveFile();
+			if (!newActiveFile || activeFile?.path != newActiveFile.path) return
+
+			const app = this.app as any
+			app.commands.executeCommandById('editor:save-file');
+			this.debounceTimer = null;
+		}, this.settings.debounceTime * 1000);
 	}
 
 	async loadSettings() {
@@ -91,44 +67,35 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AutoSaveSettingTab extends PluginSettingTab {
+	plugin: AutoSavePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoSavePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
+		containerEl.createEl('h2', { text: 'Auto Save Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Debounce time (ms)')
+			.setDesc('Time to wait after the last edit before automatically saving the active file. Minimum 0.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder(String(DEFAULT_SETTINGS.debounceTime))
+				.setValue(this.plugin.settings.debounceTime.toString())
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					const numValue = parseInt(value, 10);
+					if (!isNaN(numValue) && numValue >= 0) {
+						this.plugin.settings.debounceTime = numValue;
+						await this.plugin.saveSettings();
+					} else {
+						new Notice("Please enter a valid non-negative number for debounce time.");
+						text.setValue(this.plugin.settings.debounceTime.toString());
+					}
 				}));
 	}
 }
